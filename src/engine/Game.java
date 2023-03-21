abbe1a135a335ba96ad87b596e02ae2d35a7503e
Youssef Malaak Rasmy <128165178+usefmalaak23@users.noreply.github.com>
2023-03-21 21:44:34 +0200

    Add files via upload

---
+++ src/engine/Game.java
@@ -0,0 +1,41 @@
+package engine;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import model.characters.Hero;
+import model.characters.Explorer;
+import model.characters.Fighter;
+import model.characters.Medic;
+import model.characters.Zombie;
+import model.world.Cell;
+
+public class Game {
+	public static ArrayList<Hero> availableHeroes;
+	public static ArrayList<Hero> heroes;
+	public static ArrayList<Zombie> zombies;
+	public static Cell [][] map;
+
+	public static void loadHeros(String line) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new FileReader("heros.csv"));
+		
+		while ((line = br.readLine()) != null) {  
+			
+			String[] hero = line.split(",");
+			
+			int maxHP = Integer.parseInt(hero[2]);
+			int attackDmg = Integer.parseInt(hero[4]);
+			int maxActions  = Integer.parseInt(hero[3]);
+		
+		 switch (hero[1]){
+		 case "FIGH": availableHeroes.add(new Fighter(hero[0], maxHP, attackDmg, maxActions));
+		 break;
+		 case "EXP": availableHeroes.add(new Explorer(hero[0], maxHP, attackDmg, maxActions));
+		 break;
+		 case "MED": availableHeroes.add(new Medic(hero[0],  maxHP, attackDmg, maxActions));
+		}
+		br.close();
+		}
+}}
