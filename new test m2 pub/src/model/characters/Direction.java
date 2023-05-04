bb31814399a63ee43828c3de249fd7a03aef5c57
Yousefyasser <yousef.mostafa@student.guc.edu.eg>
2023-05-04 22:58:30 +0300

    Delete new test m2 pub directory

--- new test m2 pub/src/model/characters/Direction.java
+++
@@ -1,5 +0,0 @@
-package model.characters;
-
-public enum Direction {
-	UP, DOWN, LEFT, RIGHT;
-}
--- new test m2 pub/src/model/collectibles/Collectible.java
+++
@@ -1,9 +0,0 @@
-package model.collectibles;
-
-import model.characters.Hero;
-
-public interface Collectible {
-
-    void pickUp(Hero h);
-    void use(Hero h);
-}
